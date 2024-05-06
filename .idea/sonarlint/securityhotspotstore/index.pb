b70638332e549bb06ff9c03d4781955cfa31162c
EunjiShin <eunji980310@gmail.com>
2024-05-07 00:02:58 +0900

    [kotlin] inline

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 V
 &kotlin/src/main/kotlin/inline/Timer.kt,7/9/792790feb6930336ef8c7b3206527221f1c8619f
+Y
+)kotlin/src/main/kotlin/inline/TimeUnit.kt,f/a/fa48628d9e40e60cd50148ff4fd2947cd0fbe066
---
+++ kotlin/src/main/kotlin/inline/TimeUnit.kt
@@ -0,0 +1,17 @@
+package inline
+
+interface TimeUnit {
+    val millis: Long
+}
+
+inline class Minutes(val minutes: Long): TimeUnit {
+    override val millis: Long get() = minutes * 60 * 1000
+}
+
+inline class Millis(val milliseconds: Long): TimeUnit {
+    override val millis: Long get() = milliseconds
+}
+
+fun setUpTimer(time: TimeUnit) {
+    val millis = time.millis
+}
